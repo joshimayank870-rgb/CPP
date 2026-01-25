b84942b60af69ac3b5b9f469adc7a0b3de40a215
Mayank Joshi <joshimayank870@gmail.com>
2026-01-25 21:07:50 +0530

    Add files via upload

---
+++ second.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int a = 5;
+void sum()
+{
+  int a;
+  cout << a;
+}
+
+int main()
+{
+  int a = 3;
+  int b = 4;
+  a = 56;
+  sum;
+
+  cout << "the value of a is " << a << "\nthe value of b is " << b;
+  return 0;
+}
