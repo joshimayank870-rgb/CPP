4428098fc8ad59ac5336704a88d9de924df4220b
Mayank Joshi <joshimayank870@gmail.com>
2026-01-25 21:08:10 +0530

    Add files via upload

---
+++ third.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a; 
+    int b;
+    cout<<"enter the number a \n";
+    cin>>a;
+    cout<<"enter the number b\n";
+    cin>>b;
+    cout<<"the sum of a and  b are"<<a+b;
+    return 0;
+    
+}
