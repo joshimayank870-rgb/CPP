23391ae1dd2fcc4113cd48b0b1f82ad290945f54
Mayank Joshi <joshimayank870@gmail.com>
2026-01-26 22:40:42 +0530

    Introduce student struct and display student details
    Added a struct to store student information and updated main function to demonstrate its usage.

--- twenty.cpp
+++ twenty.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
 using namespace std;
-int main(){
-  int a[] = {30;50;70;40};
-int *b = a;
-cout<<"the 
+struct student 
+ {
+    int Sid;
+    char name;
+    float marks;
+
+ };
+ int main(){
+    struct  student mayank;
+    struct student sam;
+    sam.Sid = 8398;
+    sam.name = 's';
+    sam.marks = 53;
+    cout<<sam.Sid<<endl<<sam.name<<endl<<sam.marks<<endl;
+
+    mayank.Sid = 1099;
+    mayank.name = 'm';
+    mayank.marks = 64;
+    cout<<mayank.Sid<<endl;
+    cout<<mayank.name<<endl;
+    cout<<mayank.marks<<endl;
+
+    return 0;
+
+ }
+
